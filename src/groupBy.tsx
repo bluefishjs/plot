d954e121f223c9a68d1acd26c74a0d7b993b6b4f
Josh Pollock <jopo@mit.edu>
2024-02-01 11:56:06 -0500

    fix bar chart examples

--- src/groupBy.tsx
+++ src/groupBy.tsx
@@ -1,5 +1,5 @@
 import { withBluefish, StackH, StackV } from "@bluefish-js/solid";
-import { usePlotContext, PlotContext } from "./plot";
+import { usePlotContext, PlotContext, Plot } from "./plot";
 import { For, JSX, createEffect, createMemo } from "solid-js";
 export type GroupByProps<T> = {
   x?: keyof T;
@@ -9,20 +9,21 @@ export type GroupByProps<T> = {
   children: () => JSX.Element;
 };
 
-export const GroupBy = withBluefish(<T,>(props: GroupByProps<T>) => {
-  const plotContext = usePlotContext();
+export const GroupBy = withBluefish(
+  <T,>(props: GroupByProps<T>) => {
+    const plotContext = usePlotContext();
 
-  const data = () => props.data ?? plotContext.data;
+    const data = () => props.data ?? plotContext.data;
 
-  // TODO: now we need nested coordinate systems/scales! let's ignore that for now... it might start
-  // to become necessary when we introduce axes and legends...
+    // TODO: now we need nested coordinate systems/scales! let's ignore that for now... it might start
+    // to become necessary when we introduce axes and legends...
 
-  // grouped data is a nested array so that it can be used in the grid defined below
-  // `x` specifies the values in each row
-  // `y` specifies the values in each column
-  // `x` and `y` might be optional, in which case there is only one row or column, respectively
-  // e.g.:
-  /* 
+    // grouped data is a nested array so that it can be used in the grid defined below
+    // `x` specifies the values in each row
+    // `y` specifies the values in each column
+    // `x` and `y` might be optional, in which case there is only one row or column, respectively
+    // e.g.:
+    /* 
   Input data:
   const tinyData: {
   Fruit: string;
@@ -55,72 +56,85 @@ export const GroupBy = withBluefish(<T,>(props: GroupByProps<T>) => {
     ]
   ]
   */
-  const groupedData = createMemo(() => {
-    const groupedData: T[][][] = [];
-    const x = props.x;
-    const y = props.y;
+    const groupedData = createMemo(() => {
+      const groupedData: T[][][] = [];
+      const x = props.x;
+      const y = props.y;
 
-    if (x !== undefined && y !== undefined) {
-      const xValues = new Set(data().map((d) => d[x]));
-      const yValues = new Set(data().map((d) => d[y]));
+      if (x !== undefined && y !== undefined) {
+        const xValues = new Set(data().map((d) => d[x]));
+        const yValues = new Set(data().map((d) => d[y]));
 
-      for (const xValue of xValues) {
-        const row: T[][] = [];
-        for (const yValue of yValues) {
-          row.push(data().filter((d) => d[x] === xValue && d[y] === yValue));
+        for (const xValue of xValues) {
+          const row: T[][] = [];
+          for (const yValue of yValues) {
+            row.push(data().filter((d) => d[x] === xValue && d[y] === yValue));
+          }
+          groupedData.push(row);
         }
-        groupedData.push(row);
+      } else if (x !== undefined) {
+        const xValues = new Set(data().map((d) => d[x]));
+        for (const xValue of xValues) {
+          groupedData.push([data().filter((d) => d[x] === xValue)]);
+        }
+      } else if (y !== undefined) {
+        const yValues = new Set(data().map((d) => d[y]));
+        groupedData.push([]);
+        for (const yValue of yValues) {
+          groupedData[0].push(data().filter((d) => d[y] === yValue));
+        }
+      } else {
+        groupedData.push([data()]);
       }
-    } else if (x !== undefined) {
-      const xValues = new Set(data().map((d) => d[x]));
-      for (const xValue of xValues) {
-        groupedData.push([data().filter((d) => d[x] === xValue)]);
-      }
-    } else if (y !== undefined) {
-      const yValues = new Set(data().map((d) => d[y]));
-      groupedData.push([]);
-      for (const yValue of yValues) {
-        groupedData[0].push(data().filter((d) => d[y] === yValue));
-      }
-    } else {
-      groupedData.push([data()]);
-    }
 
-    // console.log(groupedData);
+      // console.log(groupedData);
 
-    return groupedData;
-  });
+      return groupedData;
+    });
 
-  return (
-    <StackH alignment="bottom" total={plotContext.dims.width}>
-      <For each={groupedData()}>
-        {(col) => (
-          <StackV spacing={props.ySpacing ?? 10} /* total={plotContext.dims.height} */>
-            <For each={col}>
-              {(datum) => {
-                return (
-                  <PlotContext.Provider
-                    value={{
-                      domains: plotContext.domains,
-                      setDomains: plotContext.setDomains,
-                      get data() {
-                        return datum;
-                      },
-                      scales: plotContext.scales,
-                      dims: {
-                        width: plotContext.dims.width / groupedData().length - 10,
-                        height: plotContext.dims.height / col.length,
-                      },
-                    }}
-                  >
-                    {props.children()}
-                  </PlotContext.Provider>
-                );
-              }}
-            </For>
-          </StackV>
-        )}
-      </For>
-    </StackH>
-  );
-});
+    return (
+      <StackH alignment="bottom" total={plotContext.dims.width}>
+        <For each={groupedData()}>
+          {(col) => (
+            <StackV spacing={props.ySpacing ?? 10} /* total={plotContext.dims.height} */>
+              <For each={col}>
+                {(datum) => {
+                  console.log("datum", datum);
+                  return (
+                    // <PlotContext.Provider
+                    //   value={{
+                    //     domains: plotContext.domains,
+                    //     setDomains: plotContext.setDomains,
+                    //     get data() {
+                    //       return datum;
+                    //     },
+                    //     scales: plotContext.scales,
+                    //     dims: {
+                    //       width: plotContext.dims.width / groupedData().length - 10,
+                    //       height: plotContext.dims.height / col.length,
+                    //     },
+                    //   }}
+                    // >
+                    //   {props.children()}
+                    // </PlotContext.Provider>
+                    <Plot
+                      data={datum}
+                      width={plotContext.dims.width / groupedData().length - 10}
+                      height={plotContext.dims.height / col.length}
+                      x={plotContext.scales.x}
+                      // y={plotContext.scales.y}
+                      color={plotContext.scales.color}
+                    >
+                      {props.children()}
+                    </Plot>
+                  );
+                }}
+              </For>
+            </StackV>
+          )}
+        </For>
+      </StackH>
+    );
+  },
+  { displayName: "GroupBy" }
+);
